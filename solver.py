a9a003bf02a25eb324f115bf83f880dae2fcd917
centauria <centauria_v_c@163.com>
2019-03-31 23:20:37 +0800

    move sudoku into package

--- solver.py
+++ solver.py
@@ -1,1 +1,14 @@
 # coding=utf-8
+
+from tensorflow import keras
+from sudoku import *
+
+model = keras.models.Sequential([
+	keras.layers.Dense(units=100, activation='relu', input_dim=81),
+	keras.layers.Dense(units=100, activation='relu', input_dim=100),
+	keras.layers.Dense(units=2, activation='softmax', input_dim=100)
+])
+
+s = Sudoku(10)
+s.answer()
+print(model.predict(s.data.reshape((1, -1))))
